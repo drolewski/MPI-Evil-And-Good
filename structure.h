60e94625336a9a8863e067bbb21972471e740095
Rolex <drolewski@gmail.com>
2020-06-10 17:21:37 +0200

    Add structure.h

---
+++ structure.h
@@ -0,0 +1,34 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+
+#define false 0
+#define true 1
+
+#define PREQ 10
+#define PACK 20
+#define TREQ 30
+#define TACK 40
+#define SYNCHR 50
+#define ACKALL 60
+#define REJECT 70
+
+struct Person {
+    
+}
+
+struct Object {
+
+}
+
+struct Request {
+
+}
+
+struct ARequest {
+
+}
